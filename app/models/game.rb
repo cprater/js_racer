4fd010f7d922e441cfb3aa7fd2e8b7f0ee94a44a
cprater <cprater23@gmail.com>
2014-02-24 18:44:56 -0600

    commiting to end the night. woot.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base
-  # Remember to create a migration!
+
+  has_and_belongs_to_many :users
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+
+  has_many :games
+
   validates_uniqueness_of :name
   validates :name, presence: true
   validates_length_of :name, minimum: 3, maximum: 3, allow_blank: false
